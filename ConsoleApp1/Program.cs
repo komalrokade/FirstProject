eeb1179ca9310cfec7d44e0c424ef6c5745f658c
komal.rokade <komal.rokade@AZ-VDI-D1-247>
2022-05-18 22:03:45 -0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        /// <summary>
+        /// Enter Point Method
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            //Output Statement
+            //Console is a class under System Namespace
+            Console.WriteLine("Hello World!");
+            int x, y, z;
+            Console.WriteLine("Enter the value of x");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of y");
+            y = Convert.ToInt32(Console.ReadLine());
+            z = x + y;
+            //"z" is a string
+            //+ is the operatoe which is overloade for string Concatination
+            Console.WriteLine("The value of z is" +z);
+            //inpur statement, waits for the input from the console
+            Console.ReadLine();
+        }
+    }
+}
