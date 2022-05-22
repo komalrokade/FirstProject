9b17c55a3d15d978ead349cc47dc40aaca34fc18
Komal Rokade <komal.rokade@simplifyhealthcare.com>
2022-05-22 06:09:58 -0700

    change

---
+++ PracticeQuestions/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace PracticeQuestions
+{
+    public class Program 
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //Qustion no. 2
+            //private static abstract void Display();
+            //o/p = virtual and abstract method cannot be private
+
+
+            //Questions no. 3
+            //protected abstract void display();
+            //o/p = it is possible but have to override it
+
+            //Qustion no. 4
+            //private static virtual void Show();
+            //o/p = virtual and abstract method cannot be private
+
+
+            //Qustion no. 5
+            //Program1.Count = 100;
+            //Program1.IncCount();
+            //Console.WriteLine($"new count :[0] {Program1.Count}");
+            //Console.WriteLine("Press any key");
+            //Console.ReadKey();
+            //o/p = yes we can have private constructor in public class
+
+
+            // Qustion no. 6
+            //Class obj = new Class();
+            //o/p = yes we can have static constructor in public class
+
+            //Qustion no. 7
+            
+
+        }
+
+    }
+}
+
---
+++ PracticeQuestions/Program1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeQuestions
+{
+    public abstract class Program1
+    {
+
+        //Que. 5
+        //private Program1() { }
+
+        //public static int Count;
+        //public static int IncCount()
+        //{
+        //    return ++Count;
+        //}
+
+
+        //Que. 6
+        //static Program1()
+        //{
+        //    Console.WriteLine("This is static constructor");
+        //}
+
+        //Que. 7
+    }
+        
+}
